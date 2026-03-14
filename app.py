b7e3697764c609e4fe703e85ced130bffb66fb3f
BennyBlazz-1 <bryanlazaro4khd@gmail.com>
2026-03-13 20:06:08 -0600

    Cambio en mensaje API Azure

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "API funcionando en Azure"
+    return "API actualizada y funcionando correctamente en Azure App Service! 😄"
 
 if __name__ == "__main__":
     app.run()
